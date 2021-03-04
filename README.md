d8f71a3d3f3afe354bc0710ffa3f01def66af5bd
Daria Prokofieva <daria.prokofieva@hqplus.de>
2021-03-04 17:23:48 +0100

    add README

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # hq-web-dem
+
+Commands:
+
+1. Create ENV variables
+ ```NAME=alecsa VERSION=v0.0.2```
+   
+2. Run command
+```git checkout $VERSION```
+
+3. ```npm install```
+
+4. Build the project and pass ENV variables
+```npm run build -- --env NAME=$NAME --env VERSION=$VERSION```
+git add .
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,24 @@
 const path = require('path');
 const webpack = require("webpack");
 
-module.exports = {
-    mode:'production',
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-        new webpack.DefinePlugin({
-            'process.env': {
-                NODE_ENV: JSON.stringify(process.env.NODE_ENV),
-                NAME: JSON.stringify(process.env.NAME),
-                VERSION: JSON.stringify(process.env.VERSION),
-            },
-        }),
-    ]
+module.exports = (env) => {
+    console.log('VERSION: ', env.VERSION);
+    console.log('NAME: ', env.NAME);
+    return {
+        mode: 'production',
+        entry: './src/index.js',
+        output: {
+            filename: 'main.js',
+            path: path.resolve(__dirname, 'dist'),
+        },
+        plugins: [
+            new webpack.DefinePlugin({
+                'process.env': {
+                    NODE_ENV: JSON.stringify(env.NODE_ENV),
+                    NAME: JSON.stringify(env.NAME),
+                    VERSION: JSON.stringify(env.VERSION),
+                },
+            }),
+        ]
+    }
 };

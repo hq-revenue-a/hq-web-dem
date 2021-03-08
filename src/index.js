1ab72cf54d63ef9ebdbdac5d1603b6910e082dd3
Paulo Barbeiro <paulo.barbeiro@paulos-mbp.localdomain>
2021-03-09 00:21:23 +0100

    implements checkout functionality

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 const name = process.env.NAME || 'no-name';
-const hotel = process.env.HOTEL || 'no-hotel-name';
 const version = process.env.VERSION || 'no-version';
 
 function main() {
-    alert(`Hello ${name} I use version ${version}: hotel: ${hotel}`)
+    alert(`Hello ${name} I use version ${version}`)
 }
 
 main();

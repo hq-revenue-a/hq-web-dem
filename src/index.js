ed1bcdca98b6766dd3263884768a913b9f6fa807
Paulo Barbeiro <paulo.barbeiro@paulos-mbp.localdomain>
2021-03-09 00:26:03 +0100

    implements a simulated version 0.6

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 const name = process.env.NAME || 'no-name';
+const id = process.env.HOTEL_ID || 'no-id';
 const version = process.env.VERSION || 'no-version';
 
 function main() {
-    alert(`Hello ${name} I use version ${version}`)
+    alert(`Hello ${name}:${id} I use version ${version}`)
 }
 
 main();
